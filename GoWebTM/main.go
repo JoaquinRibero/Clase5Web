852bb5f164d5d0434e8027675c2b32db172917df
Joaquin Ribero <joaquin.ribero@mercadolibre.com>
2021-12-22 12:09:36 -0300

    Ejercicios 1 y 2

--- GoWebTM/main.go
+++ GoWebTM/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import "github.com/gin-gonic/gin"
+
+func main() {
+
+	router := gin.Default()
+
+	router.GET("/:name", func(ctx *gin.Context) {
+		name := ctx.Param("name")
+		ctx.JSON(200, gin.H{
+			"message": "Hola " + name,
+		})
+	})
+	router.Run(":3000")
+
+}
